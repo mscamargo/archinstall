ec42602094c1c6b04d72327c0b5d00da687669c1
mscamargo <mscamargo@pm.me>
2022-03-03 21:10:12 -0300

    add more packages and remove bash files from home

--- post-install.sh
+++ post-install.sh
@@ -4,4 +4,9 @@ cd $HOME
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg --noconfirm -si
-yay -S --noconfirm brave-bin
+
+yay -S xorg xorg-xinit wget tmux brave-bin zsh-completions
+
+# Remove bash stuff
+cd ~/
+rm .bash*
